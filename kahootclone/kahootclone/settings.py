6ff5b052b7292de62ffcca5316de22f74214060f
manuloseta <manuloseta@gmail.com>
2023-04-09 16:55:27 +0200

    render minor changes 4

--- kahootclone/kahootclone/settings.py
+++ kahootclone/kahootclone/settings.py
@@ -134,10 +134,10 @@ LOGIN_REDIRECT_URL = '/'
 # subject to change later depending on where we put them
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
+# STATIC_ROOT = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
