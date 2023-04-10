d7aeb0a7fbd3d2c05364ec8443ee68db1b9870e9
manuloseta <manuloseta@gmail.com>
2023-04-10 15:00:54 +0200

    oops forgot collectstatic

--- kahootclone/build.sh
+++ kahootclone/build.sh
@@ -4,6 +4,7 @@ set -o errexit  # exit on error
 
 pip install -r requirements.txt
 
+python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
 python manage.py populate
