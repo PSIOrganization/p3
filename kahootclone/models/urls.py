f59eafe259c8fecc2a8e01faf78536cbafdbd597
Elena <elenabalseirogarcia2001@gmail.com>
2023-04-21 12:35:11 +0200

    moved to restServer

--- kahootclone/models/urls.py
+++ kahootclone/models/urls.py
@@ -5,16 +5,7 @@ Contains all the urls relevant to the kahootclone project
 
 from django.urls import path, include
 from . import views
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-
-# Here we will add all urls relevant to the models app
-router.register('participant', views.ParticipantViewSet, 'participant')
-router.register('games', views.GameViewSet, 'game')
-router.register('guess', views.GuessViewSet, 'guess')
 
 urlpatterns = [
      path('signup', views.SignUp.as_view(), name='signup'),
-     path('api/', include(router.urls)),
 ]
--- kahootclone/restServer/urls.py
+++ kahootclone/restServer/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path, include
-from services import views
+from . import views
 from rest_framework import routers
 
+router = routers.DefaultRouter()
+
+# Here we will add all urls relevant to the models app
+router.register('participant', views.ParticipantViewSet, 'participant')
+router.register('games', views.GameViewSet, 'game')
+router.register('guess', views.GuessViewSet, 'guess')
+
+
 # En el router vamos agnadiendo los endpoints a los viewsets
-urlpatterns = []
+urlpatterns = [  
+    path('', include(router.urls))
+]
 
 # Here we will be registering the endpoints
