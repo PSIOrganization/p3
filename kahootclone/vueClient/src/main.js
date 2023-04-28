7b1565ba7a381378f94016f60db3cfc6b7057bad
Elena <elenabalseirogarcia2001@gmail.com>
2023-04-28 10:37:15 +0200

    createStore en main.js

--- kahootclone/vueClient/src/main.js
+++ kahootclone/vueClient/src/main.js
@@ -1,14 +1,33 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import {createStore} from 'vuex'
 
 import './assets/main.css'
 
 const app = createApp(App)
 
+const store = createStore({
+    state(){
+        return{
+            uuidp: ''
+        }
+    },
+    mutations: {
+        storeparticipant(state, {uuidp}){
+            state.uuidp = uuidp
+        }
+    }
+})
+
 app.use(router)
+app.use(store)
 
 app.mount('#app')
 
 import '../node_modules/bootstrap/dist/js/bootstrap.js'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+// store.commit('storeparticipant',{
+//     'uuidp': 'value'
+// });
